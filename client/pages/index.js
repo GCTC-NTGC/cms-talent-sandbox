6afd18d8dcfb07775a7e9019edc402ad6208d058
Matt Nigh <matt@mattnigh.com>
2021-04-20 12:49:31 -0400

    Update index getStaticProps test

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+function Home({ users }) {
   return (
     <div>
       <Head>
@@ -9,10 +9,30 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Hello World, Next.js
-        </h1>
+        <h1>getStaticProps test</h1>
+        <ol>
+        { users && users.map((user, key) => {
+          return <li key={key}>{user.name}</li>;
+        })}
+        </ol>
       </main>
     </div>
   )
 }
+
+export async function getStaticProps() {
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+  const users = await res.json()
+
+  if (!users) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { users },
+  }
+}
+
+export default Home
