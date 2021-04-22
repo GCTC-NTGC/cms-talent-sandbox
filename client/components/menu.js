802048edaa5cc431afd1c377e882f60c302ff65c
Matt Nigh <matt@mattnigh.com>
2021-04-22 15:21:09 -0400

    Add users page

--- client/components/menu.js
+++ client/components/menu.js
@@ -5,6 +5,7 @@ function Menu() {
     <ul>
         <li><Link href="/"><a>Home</a></Link></li>
         <li><Link href="/posts"><a>Posts</a></Link></li>
+        <li><Link href="/users"><a>Users</a></Link></li>
     </ul>
   )
 }
