edeee595cdf0ee7a9a8f79a5ceb3c74d97daa3e5
Yoni K <yoni.kid15@gmail.com>
2021-05-14 11:56:31 -0400

    Remove asset helper from links

--- lumen-react/resources/views/home.blade.php
+++ lumen-react/resources/views/home.blade.php
@@ -5,9 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Put your description here.">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("/apple-touch-icon.png") }}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("images/favicon-32x32.png") }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("images/favicon-16x16.png") }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png")>
   <link rel="manifest" href="/site.webmanifest">
   <link href="{{ mix('css/hydrogen.css') }}" rel="stylesheet" type="text/css"/>
   <title>GC Talent</title>
@@ -15,6 +15,6 @@
 <body>
   <div id="app"></div>
   <script src="{{ mix("js/app.js") }}"></script>
-  <img src="{{ asset("images/logo_canada_colour.png") }}" alt="">
+  <img src="images/logo_canada_colour.png" alt="">
 </body>
 </html>
