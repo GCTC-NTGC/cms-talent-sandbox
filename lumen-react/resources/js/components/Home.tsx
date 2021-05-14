75907c10a093fd05cea4158b1ded862a754b7453
Yoni K <yoni.kid15@gmail.com>
2021-05-14 11:29:10 -0400

    Add images and favicon to homepage for testing

--- lumen-react/resources/js/components/Home.tsx
+++ lumen-react/resources/js/components/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FunctionComponent<HomeProps> = (props) => {
   return (
     <section data-h2-container="b(center, m)">
       <div
-        data-h2-bg-color="b(white)"
+        data-h2-bg-color="b(blue)"
         data-h2-padding="b(top-bottom, m) m(all, l)"
         data-h2-radius="b(s)"
         data-h2-shadow="b(s) m(m)"
@@ -14,6 +14,7 @@ const Home: React.FunctionComponent<HomeProps> = (props) => {
         <p data-h2-text-align="b(center)">
           Welcome to <span data-h2-font-color="b(black)">GC Talent</span>!
         </p>
+        <img src="images/bg-mountains.jpg" alt="" />
       </div>
     </section>
   );
